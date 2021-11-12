8bfd42545d6c0583f90418505d3096226989f79f
Mayank Gupta <aya.gup@gmail.com>
2021-11-12 15:39:20 +0000

    working copy

---
+++ demo/src/main/java/com/example/demo/JpaConfig.java
@@ -0,0 +1,17 @@
+// package com.example.demo;
+
+
+// @Configuration
+// public class JpaConfig {
+      
+//     @Bean
+//     public DataSource getDataSource() 
+//     {
+//         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//         dataSourceBuilder.driverClassName("org.h2.Driver");
+//         dataSourceBuilder.url("jdbc:h2:file:C:/temp/test");
+//         dataSourceBuilder.username("sa");
+//         dataSourceBuilder.password("");
+//         return dataSourceBuilder.build();
+//     }
+// }
